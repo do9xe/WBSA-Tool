119972e5f4bbbc1f78683c9503f72e9fc3f70ab0
Hendrik Lüth <hendrik.lueth@cancom.de>
2023-07-31 21:50:21 +0200

    Filter-Funktion für Tabellen implementiert

--- WBSAtool/frontend/static/table_utils.js
+++ WBSAtool/frontend/static/table_utils.js
@@ -38,6 +38,21 @@ function getParentTableElement(html_element) {
     }
 }
 
+function getParentTableCell(html_element) {
+    var current_parent = html_element.parentElement;
+    while (true) {
+        if (current_parent.tagName === ("TH"||"TD")) {
+            console.log("tablecell found");
+            return current_parent;
+        }
+        if (current_parent.tagName === "BODY"){
+            console.log("reached body, aborting")
+            return;
+        }
+        current_parent = current_parent.parentElement;
+    }
+}
+
 function selectAll(current_table){
     var ele= getCheckboxesByName(current_table, 'select_row');
     for(var i=0; i<ele.length; i++){
@@ -113,3 +128,86 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 });
+
+
+// Utils for searching & filtering a table
+document.addEventListener('DOMContentLoaded', () => {
+    // first, find all divs with the name "filter-column"
+    let filter_col_list = document.getElementsByName("filter-col");
+    for (let i=0; i<filter_col_list.length; i++) {
+        //now we place a search Icon into every div with the name "filter-column"
+        const searchIcon = '<i class="bi bi-search text-secondary ms-1" onclick="displaySearch(this)"></i>';
+        filter_col_list[i].innerHTML = searchIcon;
+        filter_col_list[i].style = "display:inline;";
+    }
+});
+
+function displaySearch(sourceElement) {
+    let searchBar = '<i class="bi ms-1 bi-x-circle-fill" onclick="removeFilter(this)"></i>' +
+        '<div class="bg-light shadow-sm rounded-1" style="z-index:9;position:absolute;">' +
+        '<div class="input-group input-group-sm px-1 py-1">' +
+        '<input class="form-control" type="text" id="currentSearchInput">' +
+        '<button class="btn btn-outline-success" type="button" id="currentSearchButton" onclick="searchCol(this)"><i class="bi bi-search"></i></button>' +
+        '</div></div>';
+    sourceElement.parentElement.id = "currentSearchWindow";
+    sourceElement.outerHTML = searchBar;
+    document.getElementById("currentSearchInput").addEventListener('keydown',(e) => {
+        if (e.key === "Enter") {
+            document.getElementById("currentSearchButton").click();
+        }
+    });
+}
+
+function searchCol(sourceElement) {
+    // get the search term from the Element
+    let searchTerm = sourceElement.parentElement.getElementsByTagName("input")[0].value;
+    if (searchTerm.length === 0) {
+        return;
+    }
+    // actually filter the table
+    filterTable(sourceElement, searchTerm);
+    // after filtering, place a small pill badge with the search term in out table header
+    let searchTermIcon = '<span class="badge rounded-pill text-bg-secondary">'+ searchTerm + '<i class="bi ms-1 bi-x-circle-fill" onclick="removeFilter(this)"></i></span>';
+    let x = document.getElementById("currentSearchWindow");
+    x.innerHTML = searchTermIcon;
+    x.id = "";
+}
+
+function removeFilter(sourceElement) {
+    //show the full table
+    showFullTable(sourceElement);
+    // now re-place the search icon
+    const searchIcon = '<i class="bi bi-search text-secondary ms-1" onclick="displaySearch(this)"></i>';
+    if (sourceElement.parentElement.nodeName === "DIV") {
+        sourceElement.parentElement.innerHTML = searchIcon;
+    }
+    else {
+        sourceElement.parentElement.outerHTML = searchIcon;
+    }
+}
+
+function filterTable(sourceElement, searchTerm) {
+    // find out which table we are in before we cut the roots
+    let table = getParentTableElement(sourceElement);
+    let colIndex = getParentTableCell(sourceElement).cellIndex;
+    let rows = table.getElementsByTagName("tr");
+    for (let i=0; i<rows.length;i++) {
+        let this_td = rows[i].getElementsByTagName("td")[colIndex];
+        if (this_td) {
+            let txtValue = this_td.textContent || this_td.innerText;
+            if (txtValue.toUpperCase().indexOf(searchTerm.toUpperCase()) > -1) {
+                rows[i].style.display = "";
+            } else {
+                rows[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function showFullTable(sourceElement) {
+    let table = getParentTableElement(sourceElement);
+    let rows = table.getElementsByTagName("tr");
+    for (let i=0; i<rows.length;i++) {
+        rows[i].style.display = "";
+    }
+}
